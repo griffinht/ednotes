84cd05edcde7a9814baf18ac0cb9f5e14bbaa93d
stzups <griffinht@gmail.com>
2022-02-11 14:04:01 -0500

    file is read

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,20 @@
-const inputButton = document.getElementById("inputButton") as HTMLInputElement;
-inputButton.addEventListener("change", (e) => {
-    console.log(e.target.)
+const fileUpload = document.getElementById("fileUpload") as HTMLInputElement;
+fileUpload.addEventListener("change", (e) => {
+    let target = e.target as HTMLInputElement;
+    if (target.files != null) {
+        for (let file of target.files) {
+            console.log(file);
+            let fileReader = new FileReader();
+            fileReader.addEventListener("load", (e) => {
+                if (e.target != null) {
+                    console.log(e.target);
+                    if (e.target.result instanceof ArrayBuffer) {
+                        console.log(e.target.result);
+                    }
+                }
+            })
+            fileReader.readAsArrayBuffer(file);
+        }
+    }
 })
 console.log("hello world")
