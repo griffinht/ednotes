a1ced49a0c0b5d6fefea8d0c072b83fc33307ceb
stzups <griffinht@gmail.com>
2022-02-12 17:56:49 -0500

    add openVideo function

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-const fileUpload = document.getElementById("fileUpload") as HTMLInputElement;
+/*const fileUpload = document.getElementById("fileUpload") as HTMLInputElement;
 fileUpload.addEventListener("change", (e) => {
     let target = e.target as HTMLInputElement;
     if (target.files != null) {
@@ -16,5 +16,36 @@ fileUpload.addEventListener("change", (e) => {
             fileReader.readAsArrayBuffer(file);
         }
     }
-})
-console.log("hello world")
+})*/
+const footer = document.getElementsByTagName("footer")[0];
+function openVideo(parent: HTMLElement, child: HTMLElement) {
+    let main = document.createElement("main");
+    {
+        let header = document.createElement("header");
+        {
+            let button = document.createElement("button");
+            button.innerText = "x";
+            button.addEventListener("click", () => {
+                main.remove();
+            })
+            header.append(button);
+        }
+        main.append(header);
+    }
+    {
+        let div = document.createElement("div");
+        div.innerText = "video";
+        main.append(div);
+    }
+    {
+        let section = document.createElement("section");
+        for (let i = 0; i < 5; i++) {
+            let div = document.createElement("div");
+            div.innerText = "section div";
+            section.append(div)
+        }
+        main.append(section);
+    }
+    parent.insertBefore(main, child);
+}
+openVideo(document.body, footer);
