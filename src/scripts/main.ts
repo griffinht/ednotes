a7494828ac5fa26f72f11269aa16ddacc28a78e3
stzups <griffinht@gmail.com>
2022-02-13 13:47:40 -0500

    remove old codee

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,28 +4,7 @@ import "./newModal/FileInput.js";
 import "./newModal/ScreenInput.js";
 import "./newModal/UrlInput.js";
 
-
-/*const fileUpload = document.getElementById("fileUpload") as HTMLInputElement;
-fileUpload.addEventListener("change", (e) => {
-    let target = e.target as HTMLInputElement;
-    if (target.files != null) {
-        for (let file of target.files) {
-            console.log(file);
-            let fileReader = new FileReader();
-            fileReader.addEventListener("load", (e) => {
-                if (e.target != null) {
-                    console.log(e.target);
-                    if (e.target.result instanceof ArrayBuffer) {
-                        console.log(e.target.result);
-                    }
-                }
-            })
-            fileReader.readAsArrayBuffer(file);
-        }
-    }
-})*/
 const footer = document.getElementsByTagName("footer")[0];
-
 
 const nav = document.getElementsByTagName("nav")[0];
 export function addVideo(video: Video) {
