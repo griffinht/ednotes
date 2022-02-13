98bc020cacf666ad29faab9555cc1f737d41da28
stzups <griffinht@gmail.com>
2022-02-12 21:21:06 -0500

    simplify modal close

--- src/scripts/Modal.ts
+++ src/scripts/Modal.ts
@@ -1,9 +1,6 @@
 for (let element of document.getElementsByClassName("modal")) {
     let modal = element as HTMLElement;
-    let elements = modal.getElementsByTagName("button");
-    if (elements.length > 0) {
-        elements[0].addEventListener("click", () => {
-            modal.style.display = "none";
-        });
-    }
+    modal.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
 }
