c3ddc6f574a2b5653882e7db70e1e7615cd660b5
stzups <griffinht@gmail.com>
2022-02-12 21:41:37 -0500

    enter to submit, simplify

--- src/scripts/New.ts
+++ src/scripts/New.ts
@@ -4,3 +4,11 @@ const newModal = document.getElementById("newModal")!;
 document.getElementById("newModalOpen")!.addEventListener("click", () => {
     newModal.style.display = "flex";
 })
+
+const urlInput = document.getElementById("urlInput")!;
+urlInput.addEventListener("keypress", (e) => {
+    if (e.code !== "Enter") {
+        return;
+    }
+    alert("submit");
+})
