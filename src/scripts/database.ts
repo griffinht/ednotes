34b73ce3295fc2e1f31661a773c85ee0f9284eed
stzups <griffinht@gmail.com>
2022-02-13 18:38:59 -0500

    maybe fix database

--- src/scripts/database.ts
+++ src/scripts/database.ts
@@ -7,32 +7,59 @@ export interface Database {
 }
 class IndexedDatabase implements Database {
     database: IDBDatabase
-    videoStore: IDBObjectStore
 
     constructor(database: IDBDatabase) {
         this.database = database;
-        this.videoStore = this.database.createObjectStore("videoStore");
     }
 
     async getVideos(): Promise<Video[]> {
-        return (await this.videoStore.getAll()).result as Video[];
+        let request = this.database.transaction("videos", "readwrite").objectStore("videos").getAll();
+        return new Promise((resolve, reject) => {
+            request.addEventListener("success", () => {
+                resolve(request.result as Video[])
+            });
+            request.addEventListener("error", (e) => {
+                reject(e)
+            })
+        });
     }
 
     async removeVideo(id: ArrayBuffer): Promise<void> {
-        await this.videoStore.delete(id);
+        let request = this.database.transaction("videos", "readwrite").objectStore("videos").delete(id);
+        return new Promise((resolve, reject) => {
+            request.addEventListener("success", () => {
+                resolve()
+            });
+            request.addEventListener("error", (e) => {
+                reject(e)
+            })
+        });
     }
 
     async updateVideo(id: ArrayBuffer, video: Video): Promise<void> {
-        this.videoStore.put(video, id);
+        let request = this.database.transaction("videos", "readwrite").objectStore("videos").put(video, id);
+        return new Promise((resolve, reject) => {
+            request.addEventListener("success", () => {
+                resolve()
+            });
+            request.addEventListener("error", (e) => {
+                reject(e)
+            })
+        });
     }
 
 }
 export async function loadDatabase(): Promise<Database> {
     let database = await new Promise<IDBDatabase>((resolve, reject) => {
+        indexedDB.deleteDatabase("ednotes");
         let open = indexedDB.open("ednotes");
         open.addEventListener("error", (e) => {
             reject(e);
         });
+        open.addEventListener("upgradeneeded", () => {
+            let database = open.result;
+            database.createObjectStore("videos");
+        })
         open.addEventListener("success", () => {
             resolve(open.result);
         });
