517a106b7bb448e0cb609e5ad2a1baf4f7db3fd7
stzups <griffinht@gmail.com>
2022-02-13 09:19:23 -0500

    fix compiliation error

--- src/scripts/NewModal.ts
+++ src/scripts/NewModal.ts
@@ -3,46 +3,46 @@ const newModal = document.getElementById("newModal")!;
 
 newModal.addEventListener("click", (e) => {
     if (e.target === newModal) {
-        close()
+        closeModal()
     }
 });
 
-function close() {
+function closeModal() {
     urlInput.blur();
     newModal.style.display = "none";
     urlInput.value = "";
 }
 
-function open() {
+function openModal() {
     newModal.style.display = "flex";
     urlInput.focus();
 }
 
 document.getElementById("newModalOpen")!.addEventListener("click", () => {
-    open();
+    openModal();
 })
 
 const urlInput = document.getElementById("urlInput") as HTMLInputElement;
 urlInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         console.log(urlInput.value);
-        close();
+        closeModal();
     }
 })
 urlInput.addEventListener("keyup", (e) => {
     if (e.key === "Escape") {
-        close();
+        closeModal();
     }
 })
 
 const fileInput = document.getElementById("fileInput") as HTMLInputElement;
 fileInput.addEventListener("change", () => {
     if (fileInput.files === null) {
-        close();
+        closeModal();
         return;
     }
     for (let file of fileInput.files) {
         console.log(file)
     }
-    close();
+    closeModal();
 })
