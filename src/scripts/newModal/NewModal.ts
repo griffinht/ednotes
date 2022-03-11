dadfdc5f0c3b88f632c2232279c346082ae1a45c
stzups <griffinht@gmail.com>
2022-03-11 10:42:24 -0500

    add slash kb shortcut

--- src/scripts/newModal/NewModal.ts
+++ src/scripts/newModal/NewModal.ts
@@ -7,12 +7,18 @@ export class NewModal {
         this.urlInput = document.getElementById("urlInput") as HTMLInputElement;
         this.element.addEventListener("click", (e) => {
             if (e.target === this.element) {
-                this.closeModal()
+                this.closeModal();
             }
         });
         document.getElementById("newModalOpen")!.addEventListener("click", () => {
             this.openModal();
         })
+        document.addEventListener("keypress", (e) => {
+          if (e.code === "Slash") {
+           this.openModal();
+           e.preventDefault(); 
+          }
+        });
     }
     closeModal() {
         this.element.style.display = "none";
