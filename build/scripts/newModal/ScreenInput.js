76fce442c7baae835fd9507680c26c6ed8c3e966
ZeeshawnH <63985226+ZeeshawnH@users.noreply.github.com>
2022-03-24 10:58:48 -0400

    removed build directory

--- build/scripts/newModal/ScreenInput.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-document.getElementById("screenInput").addEventListener("click", (e) => {
-    console.log("capture", e);
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiU2NyZWVuSW5wdXQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvc2NyaXB0cy9uZXdNb2RhbC9TY3JlZW5JbnB1dC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsUUFBUSxDQUFDLGNBQWMsQ0FBQyxhQUFhLENBQUUsQ0FBQyxnQkFBZ0IsQ0FBQyxPQUFPLEVBQUUsQ0FBQyxDQUFDLEVBQUUsRUFBRTtJQUNwRSxPQUFPLENBQUMsR0FBRyxDQUFDLFNBQVMsRUFBRSxDQUFDLENBQUMsQ0FBQTtBQUM3QixDQUFDLENBQUMsQ0FBQSJ9
