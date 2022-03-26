979c23ed8df95e01b8184af9fc09c0546ebb9929
griffinht <git@griffinht.com>
2022-03-26 15:04:43 -0400

    add type declarations, improve docs

--- src/scripts/common/ByteBuffer.ts
+++ src/scripts/common/ByteBuffer.ts
@@ -1,8 +1,11 @@
 /**
- * The ByteBuffer class encapsulates a buffer which can be modified with read/write methods.
- * This is useful for serializing/deserializing data.
- * A ByteBuffer can be thought of as a stream, which can be consumed either by reading or writing data.
+ * The ByteBuffer class encapsulates a buffer which can be consumed like a stream with read and write methods.
+ * This is useful for serializing and deserializing various primitives.
+ * This works by storing a position to use the DataView class to read/write data from.
  * When writing data, the underlying buffer will be automatically expanded if it fills up.
+ *
+ * Booleans are stored as an unsigned 8 bit integer, where a value of 0 is false and a value of 1 is ture.
+ * Strings are stored with the length of the string as the first few bytes, depending on the size of the string.
  */
 export default class ByteBuffer {
     position: number;
@@ -39,14 +42,14 @@ export default class ByteBuffer {
     /**
      * @returns Whether the buffer has more data to read
      */
-    hasNext() {
+    hasNext(): boolean {
         return this.position < this.view.byteLength;
     }
 
     /**
      * @returns The underlying buffer
      */
-    getBuffer() {
+    getBuffer(): ArrayBufferLike {
         if (this.view.byteLength > this.position) {
             return this.view.buffer.slice(0, this.position);
         } else {
@@ -64,7 +67,7 @@ export default class ByteBuffer {
      * @param extraLength The additional length the buffer should hold. This will be added to the current position.
      *
      */
-    private checkResize(extraLength: number) {
+    private checkResize(extraLength: number): void {
         let targetLength = this.position + extraLength;
         if (targetLength > this.view.byteLength) {
             let buffer = new ArrayBuffer(targetLength);
@@ -73,100 +76,101 @@ export default class ByteBuffer {
         }
     }
 
-    readString8() {
+    readString8(): string {
         let length = this.readUint8();
         this.position += length;
         return new TextDecoder().decode(this.view.buffer.slice(this.position - length, this.position));
     }
 
-    readString16() {
+    readString16(): string {
         let length = this.readUint16();
         this.position += length;
         return new TextDecoder().decode(this.view.buffer.slice(this.position - length, this.position));
     }
 
-    readString32() {
+    readString32(): string {
         let length = this.readUint32();
         this.position += length;
         return new TextDecoder().decode(this.view.buffer.slice(this.position - length, this.position));
     }
 
     //todo b64 operations are probably broken
-    readBase64_8() {
+    readBase64_8(): string {
         return btoa(this.readString8());
     }
 
-    readBase64_16() {
+    readBase64_16(): string {
         return btoa(this.readString8());
     }
 
-    readBase64_32() {
+    readBase64_32(): string {
         return btoa(this.readString8());
     }
 
-    readBoolean() {
+    readBoolean(): boolean {
         return this.readUint8() == 1;
     }
 
-    readInt8() {
+    readInt8(): number {
         let value = this.view.getInt8(this.position);
         this.position += 1;
         return value;
     }
 
-    readUint8() {
+    readUint8(): number {
         let value = this.view.getUint8(this.position);
         this.position += 1;
         return value;
     }
 
-    readInt16() {
+    readInt16(): number {
         let value = this.view.getInt16(this.position);
         this.position += 2;
         return value;
     }
 
-    readUint16() {
+    readUint16(): number {
         let value = this.view.getUint16(this.position);
         this.position += 2;
         return value;
     }
 
-    readInt32() {
+    readInt32(): number {
         let value = this.view.getInt32(this.position);
         this.position += 4;
         return value;
     }
 
-    readUint32() {
+    readUint32(): number {
         let value = this.view.getUint32(this.position);
         this.position += 4;
         return value;
     }
 
-    readBigInt64() {
+    readBigInt64(): BigInt {
         let value = this.view.getBigInt64(this.position);
         this.position += 8;
         return value;
     }
 
-    readBigUInt64() {
+    readBigUInt64(): BigInt {
         let value = this.view.getBigUint64(this.position);
         this.position += 8;
         return value;
     }
 
-    readFloat32() {
+    readFloat32(): number {
         let value = this.view.getFloat32(this.position);
         this.position += 4;
         return value;
     }
 
-    readFloat64() {
+    readFloat64(): number {
         let value = this.view.getFloat64(this.position);
         this.position += 8;
         return value;
     }
+
 
 
 
